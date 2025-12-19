bc1ba8bcd7a991fa9b0bdc8e70aa5ec448317405
Red-foguin <oliveirarogerio585@gmail.com>
2025-12-18 23:51:10 -0300

    adicionando conteúdos sobre repetiões. while e do while

---
+++ JavaScript/007 repetições/node_1457239e16083.tmp.js
@@ -0,0 +1,6 @@
+//console.log('tudo bem?')
+var i = 1
+while (i < 9) {
+    console.log('Me ajude a contar')
+}
+console.log('Obrigado!!')
---
+++ JavaScript/007 repetições/teste.js
@@ -0,0 +1,16 @@
+//console.log('tudo bem?')
+var i = 1
+ do {
+    console.log(`Mariana conta ${i}`)
+    i++
+} while (i < 9)
+console.log('Obrigado!!')
+
+//Ambas as soluções são aceitas e executam a mesma coisa.
+
+/*var i = 1
+while (i < 9) {
+    console.log(`Mariana conta ${i}`)
+    i++
+}
+console.log('Obrigado!!')*/
