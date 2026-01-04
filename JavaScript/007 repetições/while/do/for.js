1a43187ec67d88d3fcde6eadc1651fa544dbb138
Red-foguin <oliveirarogerio585@gmail.com>
2026-01-03 21:57:37 -0300

    Adicinando estruturas While e DoWhile

---
+++ JavaScript/007 repetições/while/do/for.js
@@ -0,0 +1,16 @@
+/*  var=1
+    while (c<=10){
+    console.log(c)
+    c++
+}
+*/
+//-----------------------
+
+//O código acima e abaixo são a mesma coisa.
+//Para depurar/debugar o código, basta selecionar o ponto que quer começar, apertar F5 e ir apertando F10.
+
+console.log('Vamos contar...')
+for (var c=1; c<=10; c++){
+ console.log(c)
+}
+console.log('CONTAMOS!!') 
